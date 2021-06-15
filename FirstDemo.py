e12d608ff6c31d692b5c3fffead5a3c8d2337338
Hackveda <admin@hackveda.in>
2021-06-15 18:44:41 +0530

    First Program

---
+++ FirstDemo.py
@@ -0,0 +1,15 @@
+# Number
+age = 31
+
+# Strings
+name = "Devanshu Shukla"
+
+# List
+profile = [name, age, 5.9] # Create heterogenous lists
+
+# For Loop
+for item in profile:
+    if item == name:
+        print(item)
+
+# If Conditions
